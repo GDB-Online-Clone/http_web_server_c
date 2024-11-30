9ea8f1fc720e8d956df2bf4a19c4144dc1000b72
박수현 <54769955+xharpenParksuhyeon@users.noreply.github.com>
2024-11-30 12:44:28 +0900

    feat(src): add HTTP POST `/stop` route and callback function (#148)
    - Added a new POST route `/stop`.
    - Implemented `stop_callback` to return a success response (`{'is_success': true}`).
    - Linked `/stop` route with `stop_callback` in the `route_table`.

--- src/main.c
+++ src/main.c
@@ -6,6 +6,24 @@
 #pragma GCC diagnostic ignored "-Wunused-function"
 
 /**
+ * @test curl -X POST http://localhost:10010/stop -H "Content-Type: application/json" -d '{"key": "value"}'
+ */
+static struct http_response *stop_callback(struct http_request request) {
+    struct http_response *response = (struct http_response *)malloc(sizeof(struct http_response));
+
+    response->http_version = HTTP_1_1;
+    response->status_code = HTTP_OK;
+    response->body = strdup("{'is_success': true}");
+    response->headers = (struct http_headers) {
+        .size = 0,
+        .capacity = 0, 
+        .items = NULL
+    };
+
+    return response;
+}
+
+/**
  * @brief Build Test 용
  * 
  * @return int 프로그램 종료 상태
@@ -13,6 +31,8 @@
 int main() {
     struct routes route_table = {};
     init_routes(&route_table);
+
+    insert_route(&route_table, "/stop", HTTP_POST, stop_callback);
 
     struct web_server app = (struct web_server) {
         .route_table = &route_table,
