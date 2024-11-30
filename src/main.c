40872d6e0862d9bd871e9b420d4ffe986798be21
박수현 <54769955+xharpenParksuhyeon@users.noreply.github.com>
2024-11-30 12:48:08 +0900

    feat(src): add HTTP POST `/input` route and callback function (#149)
    - Added a new POST route `/input`.
    - Implemented `input_callback` to return a JSON response (`{'is_success': true}`).
    - Linked `/input` route with `input_callback` in the `route_table`.

--- src/main.c
+++ src/main.c
@@ -24,6 +24,25 @@ static struct http_response *stop_callback(struct http_request request) {
 }
 
 /**
+ * @test curl -X POST http://localhost:10010/input -H "Content-Type: application/json" -d '{"key": "input_value"}'
+ */
+static struct http_response *input_callback(struct http_request request) 
+{
+    struct http_response *response = (struct http_response *)malloc(sizeof(struct http_response));
+
+    response->http_version = HTTP_1_1;
+    response->status_code = HTTP_OK;
+    response->body = strdup("{'is_success': true}");
+    response->headers = (struct http_headers) {
+        .size = 0,
+        .capacity = 0,
+        .items = NULL
+    };
+
+    return response;
+}
+
+/**
  * @brief Build Test 용
  * 
  * @return int 프로그램 종료 상태
@@ -33,6 +52,7 @@ int main() {
     init_routes(&route_table);
 
     insert_route(&route_table, "/stop", HTTP_POST, stop_callback);
+    insert_route(&route_table, "/input", HTTP_POST, input_callback);
 
     struct web_server app = (struct web_server) {
         .route_table = &route_table,
