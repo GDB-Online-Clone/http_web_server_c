d8bc4c1e3bab61bfa5985c986e0b561de32590c5
박수현 <54769955+xharpenParksuhyeon@users.noreply.github.com>
2024-11-30 18:13:26 +0900

    feat(headers): add CORS headers to HTTP 500 responses  (#157)
    - Added CORS headers (`Access-Control-Allow-Origin`, `Access-Control-Allow-Methods`, `Access-Control-Allow-Headers`) to HTTP 500 responses.
    - Improved compatibility between clients and servers, enabling better support for browser-based client requests.

--- src/http.c
+++ src/http.c
@@ -825,13 +825,17 @@ int run_web_server(struct web_server server) {
     response_500 = (struct http_response) {
         .body = NULL,
         .headers = (struct http_headers) {
-            .capacity = 0,
-            .items = NULL,
+            .capacity = 8,
+            .items = malloc(8 * sizeof(struct http_header*)),
             .size = 0
         },
         .http_version = HTTP_1_1,
         .status_code = HTTP_INTERNAL_SERVER_ERROR
     };
+
+    insert_header(&response_500.headers, "Access-Control-Allow-Origin", "*");
+    insert_header(&response_500.headers, "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    insert_header(&response_500.headers, "Access-Control-Allow-Headers", "*");
 
     response_404 = (struct http_response) {
         .body = NULL,
